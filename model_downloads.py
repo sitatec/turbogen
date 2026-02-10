93dbe18f466638f158e67f0529cd93483219689b
Sita Bérété <sita.berete.3@gmail.com>
2026-02-10 02:01:10 +0000

    LoRA fused models instead of applying at runtime

--- model_downloads.py
+++ model_downloads.py
@@ -16,13 +16,14 @@ def download_qwen_image(
 ) -> Path:
     qwen_image_2512_path = _ROOT_DIR / "Qwen-Image-2512-Lightning"
 
+    ignore_patterns = ["transformer/**", "scheduler/**"]
+    if te_quant_method:
+        ignore_patterns.append("text_encoder/**")
+
     hf_hub.snapshot_download(
         repo_id="Qwen/Qwen-Image-2512",
         local_dir=qwen_image_2512_path,
-        # We don't download the original text_encoder when a quantized version is requested
-        ignore_patterns=["text_encoder/**", "transformer/**", "scheduler/**"]
-        if te_quant_method
-        else None,
+        ignore_patterns=ignore_patterns,
     )
     hf_hub.snapshot_download(
         repo_id="sitatech/Qwen-Image-2512-Turbo-2-Steps",
@@ -42,17 +43,21 @@ def download_qwen_image_edit(
 ) -> Path:
     qwen_image_edit_2511_path = _ROOT_DIR / "Qwen-Image-Edit-2511-Lightning"
 
+    ignore_patterns = ["transformer/**"]
+    if te_quant_method:
+        ignore_patterns.append("text_encoder/**")
+
     hf_hub.snapshot_download(
-        repo_id="Qwen/Qwen-Image-Edit-2511",
+        repo_id="Qwen/Qwen-Image-2512",
         local_dir=qwen_image_edit_2511_path,
-        # We don't download the original text encoder when a quantized version is requested
-        ignore_patterns=["text_encoder/**"] if te_quant_method else None,
+        ignore_patterns=ignore_patterns,
     )
-    hf_hub.hf_hub_download(
-        repo_id="lightx2v/Qwen-Image-Edit-2511-Lightning",
-        filename="Qwen-Image-Edit-2511-Lightning-4steps-V1.0-bf16.safetensors",
-        local_dir=qwen_image_edit_2511_path / "lora",
+
+    hf_hub.snapshot_download(
+        repo_id="sitatech/Qwen-Image-Edit-2511-Lightning",
+        local_dir=qwen_image_edit_2511_path,  # download transformer subfolder
     )
+
     if te_quant_method:
         hf_hub.snapshot_download(
             repo_id=f"sitatech/Qwen2.5-VL-7B-Instruct-{te_quant_method.upper()}-Int4",
